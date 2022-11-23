44a4c1116f84e4523968fb1906fdc46f4a156d4e
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-11-23 14:17:03 -0500

    adjust the grid for smaller devices

--- src/components/WebinarPost.jsx
+++ src/components/WebinarPost.jsx
@@ -16,7 +16,7 @@ class WebinarPost extends Component {
 
   render() {
     return (
-        <div className="col-lg-4">
+        <div className="col-lg-4 col-md-6">
           <div className="new-webinar">
             <div className="new-webinar-inner">
               <a href={this.props.link} className="webinar-image">
