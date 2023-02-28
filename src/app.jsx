d3947dbe81bc59787ebb2595a50dc5a4b4ef24f5
Bree Armstrong <brianna.m.armstrong@gmail.com>
2023-02-28 13:51:27 -0500

    adjust alert at top and add hyperlink

--- src/app.jsx
+++ src/app.jsx
@@ -11,7 +11,7 @@ class Webinars extends Component {
       posts: [],
       curIndex: 6,
       prevIndex: 0,
-      maxPosts: 0
+      maxPosts: 0,
     };
   }
 
@@ -24,7 +24,7 @@ class Webinars extends Component {
       ".content-block--pageItem__inside"
     );
     console.log(postArray.length);
-    this.setState({maxPosts: postArray.length})
+    this.setState({ maxPosts: postArray.length });
 
     let i = startIndex;
     console.log(i);
@@ -34,12 +34,15 @@ class Webinars extends Component {
       tempPosts.push({
         link: post.querySelector(".ctaLink").getAttribute("href"),
         title: post.querySelector(".ctaLink").getAttribute("title"),
-        tag: post
-          .querySelector(".content-block--pageItem__metadata")
+        tag: post.querySelector(".content-block--pageItem__metadata")
           .lastElementChild.innerHTML,
         date: post.querySelector(".content-block--pageItem__metadata")
-        .firstElementChild.innerHTML,
-        webinar: (post.querySelector(".ctaLink").getAttribute("href").split("/")[4] == "" ? post.querySelector(".ctaLink").getAttribute("href").split("/")[3] : post.querySelector(".ctaLink").getAttribute("href").split("/")[4]),
+          .firstElementChild.innerHTML,
+        webinar:
+          post.querySelector(".ctaLink").getAttribute("href").split("/")[4] ==
+          ""
+            ? post.querySelector(".ctaLink").getAttribute("href").split("/")[3]
+            : post.querySelector(".ctaLink").getAttribute("href").split("/")[4],
       });
       i++;
     }
@@ -48,50 +51,46 @@ class Webinars extends Component {
   };
 
   showMore = () => {
-    console.log(this.state.tempPosts)
-      var tempIndex = this.state.curIndex;
-      this.setState(
-        { prevIndex: tempIndex, curIndex: (tempIndex += 6) },
-        () => {
-          this.getPosts(this.state.prevIndex, this.state.curIndex);
-        }
-      );
-      console.log("show more with no filter");
+    console.log(this.state.tempPosts);
+    var tempIndex = this.state.curIndex;
+    this.setState({ prevIndex: tempIndex, curIndex: (tempIndex += 6) }, () => {
+      this.getPosts(this.state.prevIndex, this.state.curIndex);
+    });
+    console.log("show more with no filter");
     console.log("click");
   };
 
-
-
   render() {
-    console.log(this.state.maxPosts)
-    console.log(this.state.tempPosts == this.state.maxPosts)
+    console.log(this.state.maxPosts);
+    console.log(this.state.tempPosts == this.state.maxPosts);
     return (
-      <div className="webinar-page">
-      <WebinarAlert/>
-        <WebinarJumbo
-          background={
-            "https://www2.arccorp.com/globalassets/homepage/redesign/slides/carousel15.jpg"
-          }
-          tag={"Fraud Webinar"}
-          link={
-            "https://www2.arccorp.com/support-training/fraud-prevention/archived-fraud-webinars/2021-archived-fraud/webinar-091621/"
-          }
-          title={
-            "An Analytical View on 100 Billion Transactions by Worldpay/FIS"
-          }
-          description={
-            "Watch this information session where Tony Ashe from WorldPay will take you through trends and provide insight into global markets"
-          }
-        />
-        <div className="row">
-          <div className="col-lg-12 webinar-header">
-            <h2 className="text-left webinar-header-inner">
-              Latest Webinars
-            </h2>
+      <div>
+        <WebinarAlert />
+        <div className="container">
+          <WebinarJumbo
+            background={
+              "https://www2.arccorp.com/globalassets/homepage/redesign/slides/carousel15.jpg"
+            }
+            tag={"Fraud Webinar"}
+            link={
+              "https://www2.arccorp.com/support-training/fraud-prevention/archived-fraud-webinars/2021-archived-fraud/webinar-091621/"
+            }
+            title={
+              "An Analytical View on 100 Billion Transactions by Worldpay/FIS"
+            }
+            description={
+              "Watch this information session where Tony Ashe from WorldPay will take you through trends and provide insight into global markets"
+            }
+          />
+          <div className="row">
+            <div className="col-lg-12 webinar-header">
+              <h2 className="text-left webinar-header-inner">
+                Latest Webinars
+              </h2>
+            </div>
           </div>
-        </div>
-        <div className="row">
-        {this.state.posts.map((post) => (
+          <div className="row">
+            {this.state.posts.map((post) => (
               <WebinarPost
                 title={post.title}
                 link={post.link}
@@ -100,17 +99,31 @@ class Webinars extends Component {
                 webinar={post.webinar}
               />
             ))}
-        </div>
-        <div className="row">
-          <div className="col-lg-12 webinar-btn">
-            <div className="ctaBtn" style={{
-                  display: this.state.posts.length == this.state.maxPosts ? "none" : "inline-block",
-                  width: "220px", cursor: "pointer"
-                }} onClick={this.showMore}>View More</div>
           </div>
+          <div className="row">
+            <div className="col-lg-12 webinar-btn">
+              <div
+                className="ctaBtn"
+                style={{
+                  display:
+                    this.state.posts.length == this.state.maxPosts
+                      ? "none"
+                      : "inline-block",
+                  width: "220px",
+                  cursor: "pointer",
+                }}
+                onClick={this.showMore}
+              >
+                View More
+              </div>
+            </div>
+          </div>
+          <YoutubeFeature
+            background="https://www2.arccorp.com/globalassets/homepage/redesign/webinar/webinar-footer-image.jpg"
+            description="Subscribe to our YouTube channel."
+            title="Stay Up to Date with the Latest Industry Knowledge."
+          />
         </div>
-        <YoutubeFeature background="https://www2.arccorp.com/globalassets/homepage/redesign/webinar/webinar-footer-image.jpg" description="Subscribe to our YouTube channel." title="Stay Up to Date with the Latest Industry Knowledge."/>
-
       </div>
     );
   }
--- src/components/WebinarAlert.jsx
+++ src/components/WebinarAlert.jsx
@@ -7,20 +7,15 @@ class WebinarAlert extends Component {
 
   render() {
     return (
-      <div className="row alert-row">
-        <div className="col-lg-12">
-          <div className="webinar-alert">
-            <div className="webinar-alert-inner">
-              <a href="">
-                Stay up to date with the latest industry knowledge by
-                subscribing to our YouTube channel. <span>Subscribe</span>{" "}
-                <i class="fab fa-youtube"></i>
-                <i
-                  style={{ fontSize: "10px" }}
-                  class="fas fa-chevron-right"
-                ></i>
-              </a>
-            </div>
+      <div className="alert-row">
+        <div className="webinar-alert">
+          <div className="webinar-alert-inner">
+            <a href="https://www.youtube.com/c/airlinesreportingcorporation">
+              Stay up to date with the latest industry knowledge by subscribing
+              to our YouTube channel. <span>Subscribe</span>{" "}
+              <i class="fab fa-youtube"></i>
+              <i style={{ fontSize: "10px" }} class="fas fa-chevron-right"></i>
+            </a>
           </div>
         </div>
       </div>
